bd0f6f9936619b58422460884253d80129bd4d89
Sami Bounouar <92109469+SamiB2@users.noreply.github.com>
2025-10-20 12:01:01 +0000

    Exo 2.3

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -4,8 +4,8 @@ AGES = [20, 21, 22, 23]
 JOHN = NAMES[0]
 PAUL = NAMES[1]
 
-JOHN_PAUL = NAMES[:2]
-GEORGE_RINGO = NAMES[2:]
+JOHN_PAUL = NAMES[:2] #everything to the left of the double mark
+GEORGE_RINGO = NAMES[2:] #everything to the right of the double mark
 REVERSE = NAMES[::-1]
 EVERY_OTHER = NAMES[::2]
 
