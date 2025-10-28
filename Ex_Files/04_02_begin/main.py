bdd1b02d02355b0da9806a9af2f2e698cd0afeca
Sami Bounouar <92109469+SamiB2@users.noreply.github.com>
2025-10-28 12:53:18 +0000

    Exo 4.2

--- Ex_Files/04_02_begin/main.py
+++ Ex_Files/04_02_begin/main.py
@@ -1,3 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) #this is how you start the Flask object in Flask. 
+
+@app.route("/")
+def hello(): 
+  return "Hello, World!"
+
+app.run(debug=True)
